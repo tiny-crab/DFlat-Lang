9793b0a8c196b02c91aed0c1af56807ad32f9e27
zakattak <zachary.jay.crabtree@gmail.com>
2015-11-16 22:01:00 -0800

    testing out the tree and parsing xml

--- plex.py
+++ plex.py
@@ -1,8 +1,37 @@
 #python program that will scan an musicxml file, and at first, output the notes it finds :)
 
-from tkinter import *
-from tk.filedialog import askopenFileName
+import xml.etree.ElementTree as ET
 
-Tk().withdraw()
-filestream = askopenFileName()
-print(filestream)
+#need to clean this up, but needs to call other functions to lex, and then parse the xml files
+def main():
+
+    #should put a line in here to get the xml file from the shell
+
+    #pass the file name in here to parse, hardcoded for now
+    pplex()
+
+def pplex():
+    try:
+        #creating a tree for the xml file
+        tree = ET.parse('Testing/HelloWorld.xml')
+    except IOError:
+        print("Can't open that file, bucko.")
+
+    #use this root to iterate through all of the children in the xml file
+    root = tree.getroot()
+
+    #giving a measure number for syntax
+    measurenum = 0
+    for measure in root.iter('measure'):
+        measurenum += 1
+        print("Measure " + str(measurenum) + " ")
+        for note in root.iter('note'):
+            pitch = note.find('pitch')
+            step = pitch.find('step')
+            if(step != 'NoneType'):
+                steptext = step.text
+                print(steptext)
+        print("|\n")
+
+
+main()
