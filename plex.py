d01765d05152374611c45c7e206601521b24a45a
zakattak <zachary.jay.crabtree@gmail.com>
2015-11-17 11:03:24 -0800

    Just finished basic data structures that hold notes

--- plex.py
+++ plex.py
@@ -2,7 +2,8 @@
 
 import xml.etree.ElementTree as ET
 
-#need to clean this up, but needs to call other functions to lex, and then parse the xml files
+#need to clean this up, but needs to call other functions to lex
+#and then parse the xml files
 def main():
 
     #should put a line in here to get the xml file from the shell
@@ -11,23 +12,24 @@ def main():
     pplex()
 
 def pplex():
+    
     try:
         #creating a tree for the xml file
         tree = ET.parse('Testing/HelloWorld.xml')
     except IOError:
         print("Can't open that file, bucko.")
 
-    #array whose elements are measures
-    song = []
-
-
-
+    #list whose elements are measures
+    song = list()
 
     #use this root to iterate through all of the children in the xml file
     root = tree.getroot()
 
     #for every measure in the song
     for measure in root.iter('measure'):
+
+        #array whose elements are notes: 'E' or 'e'
+        notesInMeasure = []
         
         #and for every note in the measure
         for note in measure.iter('note'):
@@ -35,19 +37,23 @@ def pplex():
             #get the pitch of the note (could also be a rest)
             pitch = note.find('pitch')
             
-            #try to find a note name (will return "NoneType" if a rest)
+            #try to find a note name and if it's flat
             try:
-                step = pitch.find('step')
-                if(step != 'NoneType'):
-                    try:
-                        accidental = note.find('accidental')
-                        print(get(accidental))
-                    except:
-                        steptext = step.text
-                    print(steptext, end="")
-            #if it's just a rest, skip back to the top of the note loop
+                steptext = pitch.find('step').text
             except:
-                continue           
-        print("|")
+                continue
+            try:
+                altertext = pitch.find('alter').text
+            except:
+                notesInMeasure.append(steptext)
+                continue
 
+            if(altertext == '-1'):
+                notesInMeasure.append(steptext.lower())
+            elif(altertext == '1'):
+                print("What is this hashtag doing in mah music??")
+
+        song.append(notesInMeasure)
+
+    print(song)
 main()
